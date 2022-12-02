852ceb7cae1076df651c8a92dfc332bb76d71242
YadwinderSingh112 <119565198+YadwinderSingh112@users.noreply.github.com>
2022-12-02 15:52:01 +0530

    foodie

--- foodieblog/settings.py
+++ foodieblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index.apps.IndexConfig',
     'login.apps.LoginConfig',
 ]
 
@@ -117,9 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signin", views.sign_in, name="signin"),
-    path("signout", views.Sign_out, name="signout"),
-    path("signup", views.sign_up, name="signup")
+    path("signin", views.signin, name="signin"),
+    #path("signout", views.Sign_out, name="signout"),
+    # path("signup", views.sign_up, name="signup")
 ]
